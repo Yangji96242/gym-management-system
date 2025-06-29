e041eda20db27c3e26a3c11da092592194bb1899
yangj <yangji96242@outlok.com>
2025-06-29 17:54:11 +0800

    fix: 移除未使用变量和import，修正useEffect依赖警告，兼容Vercel构建

--- gym-management-next/src/pages/api/customers/[id].js
+++ gym-management-next/src/pages/api/customers/[id].js
@@ -1,6 +1,5 @@
 import dbConnect from '../../../lib/mongodb';
 import Customer from '../../../models/Customer';
-import Checkin from '../../../models/Checkin';
 
 export default async function handler(req, res) {
   const { method } = req;
